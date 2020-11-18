91c558ba1eba4167bfdc85156866526cb66fd03b
Chris Withers <chris@simplistix.co.uk>
2020-11-18 09:02:12 +0000

    new colour map with half linear, half log

--- animated_map.py
+++ animated_map.py
@@ -6,57 +6,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from dateutil.parser import parse as parse_date
-from matplotlib.colors import LinearSegmentedColormap
+from matplotlib.colors import LinearSegmentedColormap, SymLogNorm, LogNorm
 
-from animated import parallel_render
-from constants import base_path, ltla, code, cases, specimen_date, relax_2
+from animated import parallel_render, output_types
+from constants import base_path, ltla, code, cases, specimen_date, relax_2, per100k
 from download import find_latest
 from phe import load_geoms, load_population
 
 rolling_days = 14
 
-per100k = 'per100k'
 population = 'population'
-
-transitions = [
-    dict(vmax=0,  color=(0.97, 0.97, 0.94)),
-    dict(vmax=5,  color=(255/255, 230/255, 0/255)),
-    dict(vmax=30, color=(255/255, 143/255, 31/255)),
-    dict(vmax=90, color=(0.7, 0, 0)),
-    dict(vmax=200, color=(0, 0, 0)),
-]
-
-
-def make_plot_params_lookup(ts):
-    lookup = {}
-    transition = ts[0]
-    bands = [(transition['vmax'], transition['color'])]
-    i = 0
-    transition = None
-    for vmax in range(0, 130):
-        if transition is None or (vmax > transition['vmax'] and i < len(ts)-1):
-            i += 1
-            transition = ts[i]
-            transition_vmax = transition['vmax']
-            bands.append((transition_vmax, transition['color']))
-
-        colours = [(threshold / vmax if vmax else 0, colour)
-                   for threshold, colour in bands[:-1]]
-        prev_vmax, prev_colour = bands[-2]
-        final_vmax, final_colour = bands[-1]
-        try:
-            factor = (vmax-prev_vmax)/(final_vmax-prev_vmax)
-        except ZeroDivisionError:
-            factor = 0
-        colours.append((1, tuple(
-            pc+(fc-pc)*factor for fc, pc in zip(final_colour, prev_colour)
-        )))
-
-        lookup[vmax] = LinearSegmentedColormap.from_list(f'cmap{i}', colours)
-    return lookup
-
-
-cmap_lookup = make_plot_params_lookup(transitions)
 
 
 @lru_cache
@@ -70,7 +29,11 @@ def read_data(data_date):
     return pivoted.fillna(0).rolling(rolling_days).mean().unstack().reset_index(name=per100k)
 
 
-def render_dt(data_date, frame_date, image_path):
+def round_nearest(a, nearest):
+    return (a/nearest).round(0) * nearest
+
+
+def render_dt(data_date, frame_date, image_path, vmax=200, linthresh=30):
     df = read_data(data_date)
     dt = str(frame_date.date())
     data = df[df[specimen_date] == dt]
@@ -78,19 +41,24 @@ def render_dt(data_date, frame_date, image_path):
     current_pct_geo = pd.merge(load_geoms(), data, how='outer', left_on='lad19cd',
                                right_on='Area code')
 
-    vmax = int(data[per100k].max())
-
     fig, ax = plt.subplots(figsize=(10, 10))
+
     ax = current_pct_geo.plot(
         ax=ax,
         column=per100k,
-        k=10,
-        cmap=cmap_lookup[vmax], vmin=0, vmax=vmax,
         legend=True,
-        legend_kwds={'fraction': 0.02,
-                     'format': '%.0f',
-                     'label': f'number or new cases, {rolling_days} '
-                              f'day rolling average of new cases per 100,000 people'},
+        norm=SymLogNorm(linthresh=linthresh, vmin=0, vmax=vmax, base=10),
+        cmap='inferno_r',
+        vmin=0,
+        vmax=vmax,
+        legend_kwds={
+            'fraction': 0.02,
+            'extend': 'max',
+            'format': '%.0f',
+            'ticks': np.concatenate((np.arange(0, linthresh, 10),
+                                     round_nearest(np.geomspace(linthresh, vmax, 5), 10))),
+            'label': f'{rolling_days} day rolling average of new cases per 100,000 people'
+        },
         missing_kwds={'color': 'lightgrey'},
     )
     ax.set_axis_off()
