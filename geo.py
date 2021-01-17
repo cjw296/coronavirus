29d646098ed4959c8c4ae3e90ce9f5b9f8a678c3
Chris Withers <chris@simplistix.co.uk>
2021-01-17 13:02:05 +0000

    Add better names to MSOA geoms

--- geo.py
+++ geo.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from functools import lru_cache
 
 import geopandas
+import pandas as pd
 import shapely
 from geopandas import GeoDataFrame
 
@@ -9,12 +10,13 @@ from constants import repo_path
 
 
 @lru_cache
-def geoportal_geoms(name, code_column, name_column):
+def geoportal_geoms(name, code_column=None, name_column=None):
     # geoms that comes from https://geoportal.statistics.gov.uk/
     geoms = geopandas.read_file(repo_path / 'geo' / f"{name}-shp" / f"{name}.shp")
     geoms.to_crs("EPSG:3857", inplace=True)
-    geoms.rename(columns={code_column: 'code', name_column: 'name'},
-                 errors='raise', inplace=True)
+    if code_column and name_column:
+        geoms.rename(columns={code_column: 'code', name_column: 'name'},
+                     errors='raise', inplace=True)
     return geoms
 
 
@@ -53,27 +55,35 @@ def ltla_geoms_full():
 ltla_geoms = ltla_geoms_500
 
 
+@lru_cache
+def msoa_geoms_with_names(source):
+    # from https://visual.parliament.uk/msoanames
+    names = pd.read_csv(repo_path / 'geo' / 'MSOA-Names-1.10.csv',
+                        usecols=['msoa11cd', 'msoa11hclnm'])
+    geoms = geoportal_geoms(source)
+    with_names = pd.merge(geoms, names, left_on='MSOA11CD', right_on='msoa11cd')
+    with_names.rename(
+        columns={'MSOA11CD': 'code', 'msoa11hclnm': 'name'},
+        errors='raise', inplace=True
+    )
+    return with_names
+
+
 def msoa_geoms_200():
-    return geoportal_geoms(
-        "Middle_Layer_Super_Output_Areas__December_2011__EW_BSC_V2",
-        code_column='MSOA11CD',
-        name_column='MSOA11NM',
+    return msoa_geoms_with_names(
+        "Middle_Layer_Super_Output_Areas__December_2011__EW_BSC_V2"
     )
 
 
 def msoa_geoms_20():
-    return geoportal_geoms(
-        "Middle_Layer_Super_Output_Areas__December_2011__EW_BGC_V2",
-        code_column='MSOA11CD',
-        name_column='MSOA11NM',
+    return msoa_geoms_with_names(
+        "Middle_Layer_Super_Output_Areas__December_2011__EW_BGC_V2"
     )
 
 
 def msoa_geoms_full():
-    return geoportal_geoms(
-        "Middle_Layer_Super_Output_Areas__December_2011__Boundaries_Full_Extent__BFE__EW_V3",
-        code_column='MSOA11CD',
-        name_column='MSOA11NM',
+    return msoa_geoms_with_names(
+        "Middle_Layer_Super_Output_Areas__December_2011__Boundaries_Full_Extent__BFE__EW_V3"
     )
 
 
