d5f909fab93bc8d5adce18b2ecfb978b8b3dfdb7
Chris Withers <chris@simplistix.co.uk>
2021-01-19 06:47:38 +0000

    split vaccination stuff out into its own module

--- phe.py
+++ phe.py
@@ -3,19 +3,15 @@ from datetime import timedelta, date, datetime
 from functools import lru_cache
 
 import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
 from matplotlib.dates import DayLocator, DateFormatter
-from matplotlib.gridspec import GridSpec
 from matplotlib.ticker import MaxNLocator, StrMethodFormatter, FuncFormatter
 
 import series as s
 from constants import (
     base_path, specimen_date, area, cases, per100k, date_col, area_code, population,
     area_name, new_cases_by_specimen_date, pct_population, second_wave, nation, region,
-    ltla, utla, code, unique_people_tested_sum, first_dose_weekly,
-    first_vaccination, second_dose_weekly, national_lockdowns, area_type, complete_dose_daily_cum,
-    first_dose_daily_cum, second_dose_daily_cum, repo_path
+    ltla, utla, code, unique_people_tested_sum, national_lockdowns
 )
 from download import find_latest, find_all
 from geo import ltla_geoms
@@ -367,175 +363,3 @@ def plot_summary(ax=None, data_date=None, frame_date=None, earliest_date=None, t
 
     xaxis = tests_ax.get_xaxis()
     xaxis.label.set_visible(False)
-
-
-def latest_raw_vaccination_data():
-    new_weekly_path, new_weekly_dt = find_latest('vaccination_????-*', date_index=-1)
-    cum_path, cum_dt = find_latest('vaccination_cum_*', date_index=-1)
-    assert cum_dt == new_weekly_dt, f'{cum_dt} != {new_weekly_dt}'
-    new_weekly_df = read_csv(new_weekly_path)
-    cum_df = read_csv(cum_path)
-    raw = pd.merge(new_weekly_df, cum_df, how='outer',
-                   on=[date_col, area_type, area_code, area_name]).sort_values(
-        [date_col, area_code])
-    # this isn't currently populated:
-    assert raw[complete_dose_daily_cum].isnull().all()
-    return raw, new_weekly_dt
-
-
-def weekly_data(raw, nation_codes):
-    weekly = raw[[date_col, area_code, first_dose_weekly, second_dose_weekly]].dropna()
-
-    # data massaging:
-    initial = pd.DataFrame(
-        {date_col: pd.to_datetime(first_vaccination), first_dose_weekly: 0, second_dose_weekly: 0},
-        index=nation_codes
-    )
-    initial.index.name = area_code
-
-    to_fudge = weekly.set_index(date_col).sort_index().loc[:'2020-12-20'].reset_index()
-    fudged = to_fudge.groupby(area_code).agg(
-        {date_col: 'max', first_dose_weekly: 'sum', second_dose_weekly: 'sum'}
-    )
-
-    normal = weekly.set_index(date_col).sort_index().loc['2020-12-21':]
-    data = pd.concat((df.reset_index() for df in (initial, fudged, normal)))
-    data.rename(columns={first_dose_weekly: 'first_dose', second_dose_weekly: 'second_dose'},
-                  errors='raise', inplace=True)
-    return data
-
-
-def daily_data(raw, weekly):
-    initial_daily = weekly.groupby(area_code).agg({
-        'first_dose': 'sum', 'second_dose': 'sum', date_col: 'max'
-    }).reset_index()
-
-    daily_rows = raw[[date_col, area_code, first_dose_daily_cum, second_dose_daily_cum]].dropna()
-    daily_rows.rename(
-        columns={first_dose_daily_cum: 'first_dose', second_dose_daily_cum: 'second_dose'},
-        errors='raise', inplace=True)
-    daily = pd.concat(
-        [initial_daily, daily_rows[daily_rows[date_col] > initial_daily[date_col].max()]])
-
-    return daily.set_index([date_col, area_code]).groupby(area_code).diff().dropna().reset_index()
-
-
-def vaccination_dashboard():
-    # input data:
-    raw, data_date = latest_raw_vaccination_data()
-    names_frame = raw[[area_code, area_name]].drop_duplicates()
-    nation_codes = names_frame[area_code]
-    nation_populations = load_population().loc[nation_codes]
-    total_population = nation_populations.sum()[0]
-
-    weekly = weekly_data(raw, nation_codes)
-    daily = daily_data(raw, weekly)
-    all_data = pd.concat([weekly, daily])
-
-    # look out for weirdness
-    assert (all_data['first_dose'] >= 0).all()
-    assert (all_data['second_dose'] >= 0).all()
-
-    all_data['start'] = all_data[date_col].shift(len(nation_codes))
-    all_data['duration'] = all_data[date_col] - all_data['start']
-    all_data = all_data.set_index([date_col, area_code])
-    all_data['any'] = all_data.groupby(level=-1)['first_dose'].cumsum()
-    all_data['full'] = all_data.groupby(level=-1)['second_dose'].cumsum()
-    all_data['partial'] = all_data['any'] - all_data['full']
-    data = pd.merge(all_data.reset_index(), names_frame, on=area_code)
-    max_date = data[date_col].max()
-
-    # data for plotting:
-    latest = data[[area_name, area_code, 'full', 'any', 'partial']][
-        data[date_col] == max_date].copy()
-    latest = pd.merge(latest, nation_populations, on=area_code)
-
-    latest['full_pct'] = 100 * latest['full'] / latest[population]
-    latest['partial_pct'] = 100 * latest['partial'] / latest[population]
-    latest['none_pct'] = 100 - latest['full_pct'] - latest['partial_pct']
-
-    pie_data = latest.set_index(area_name).sort_index()
-    pie_data = pie_data[['full_pct', 'partial_pct', 'none_pct']].transpose()
-    pct_total = 100 * (
-        data.pivot_table(values='any', index=[date_col], columns=area_name).fillna(0)
-        / total_population
-    )
-
-    # plotting
-    england_col = 0
-    ni_col = 6
-    scotland_col = 2
-    wales_col = 3
-    colors = [plt.cm.tab10(i) for i in [england_col, ni_col, scotland_col, wales_col]]
-
-    fig = plt.figure(figsize=(16, 8.5), dpi=100)
-    fig.set_facecolor('white')
-    fig.suptitle(f'COVID-19 Vaccination Progress in the UK as of {max_date:%d %b %Y}', fontsize=14)
-
-    gs = GridSpec(3, 4, height_ratios=[1, 1, 1])
-    gs.update(top=0.95, bottom=0.15, right=0.95, left=0.02, wspace=0, hspace=0.2)
-
-    for x, nation in enumerate(pie_data):
-        ax = plt.subplot(gs[0, x])
-        ax.add_patch(plt.Circle((0, 0), radius=1, color='k', fill=False))
-        pie_data.plot(ax=ax, y=nation,
-                      kind='pie', labels=None, legend=False, startangle=-90, counterclock=False,
-                      colors=['green', 'lightgreen', 'white'],
-                      )
-        pct = pie_data[nation].loc['full_pct'] + pie_data[nation].loc['partial_pct']
-        ax.text(0, 0.5, f"{pct:.1f}%", ha='center', va='top', weight='bold', fontsize=14)
-
-    ax = plt.subplot(gs[1, :])
-    ax.yaxis.set_label_position("right")
-    ax.yaxis.tick_right()
-    ax.yaxis.grid(False)
-    ax.yaxis.set_major_formatter(StrMethodFormatter('{x:,.1f}%'))
-    plt.setp(ax.get_xticklabels(), visible=False)
-    ax.set_title('Percentage of UK population partially or fully vaccinated')
-
-    labels = [f"{nation}: {latest[latest[area_name] == nation]['any'].item():,.0f} people" for
-              nation in pct_total.columns]
-    ax.stackplot(pct_total.index, pct_total.values.transpose(), colors=colors, labels=labels)
-    ax.legend(loc='upper left')
-
-    ax = plt.subplot(gs[2, :], sharex=ax)
-    ax.yaxis.set_label_position("right")
-    ax.yaxis.tick_right()
-    ax.yaxis.set_major_formatter(FuncFormatter(lambda y, pos: f"{y / 1_000_000:.1f}m"))
-    ax.set_xlim(first_vaccination, max_date)
-    ax.set_xticks(all_data.index.get_level_values(0).unique(), minor=True)
-    major_ticks = list(weekly[date_col].unique())
-    if max_date > major_ticks[-1]+np.timedelta64(1,'D'):
-        major_ticks.append(max_date.to_datetime64())
-    ax.set_xticks(major_ticks)
-    ax.xaxis.set_major_formatter(DateFormatter('%d %b %y'))
-    ax.xaxis.label.set_visible(False)
-    ax.set_title('Rate of injections per week')
-
-    bottom = None
-    for nation_name, color in zip(pct_total, colors):
-        nation_data = data[data[area_name] == nation_name].iloc[1:].set_index(date_col)
-        if bottom is None:
-            bottom = pd.Series(0, nation_data.index)
-        heights = (nation_data['first_dose'] + nation_data['second_dose']) * (
-                    7 / nation_data['duration'].dt.days)
-        ax.bar(
-            nation_data.index - nation_data['duration'],
-            bottom=bottom,
-            height=heights,
-            width=nation_data['duration'].dt.days,
-            align='edge',
-            color=color,
-        )
-        bottom += heights
-
-    ax.set_ylim(0, 2_500_000)
-
-    fig.text(0.5, 0.08,
-             f'@chriswithers13 - '
-             f'data from https://coronavirus.data.gov.uk/ retrieved on {data_date:%d %b %Y}',
-            ha='center')
-
-    # return latest data so it gets displayed
-    plt.savefig(repo_path / f'vaccination.png', bbox_inches='tight')
-    return latest
