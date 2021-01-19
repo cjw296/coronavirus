20a451e0c3c00f46ec73534752d61d20041120e4
Chris Withers <chris@simplistix.co.uk>
2021-01-19 17:38:46 +0000

    add in lockdown 3 start

--- args.py
+++ args.py
@@ -4,7 +4,7 @@ from dateutil.parser import parse as parse_date
 
 from constants import (
     data_start, second_wave, lockdown2, earliest_available_download,
-    earliest_testing, earliest_msoa
+    earliest_testing, earliest_msoa, lockdown3
 )
 
 special_dates = {text: dt for dt, text in (
@@ -12,6 +12,7 @@ special_dates = {text: dt for dt, text in (
     (second_wave, 'second-wave'),
     (lockdown2[0], 'lockdown-2-start'),
     (lockdown2[1], 'lockdown-2-end'),
+    (lockdown3[0], 'lockdown-3-start'),
     (earliest_available_download, 'earliest-download'),
     (earliest_msoa, 'earliest-msoa'),
     (earliest_testing, 'earliest-testing'),
