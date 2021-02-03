dd83e8db9d60db73a6f4dbb65369fa9235d8a272
Chris Withers <chris@simplistix.co.uk>
2021-02-03 07:42:18 +0000

    use constants here to make sure the names are the name

--- series.py
+++ series.py
@@ -1,5 +1,5 @@
 from dataclasses import dataclass
-
+import constants as c
 
 @dataclass
 class Series:
@@ -27,78 +27,78 @@ class Series:
 
 
 new_cases_sum = Series(
-    metric='newCasesBySpecimenDateRollingSum',
+    metric=c.new_cases_sum,
     label='cases',
     color='red',
 )
 
 
 new_admissions_sum = Series(
-    metric='newAdmissionsRollingSum',
+    metric=c.new_admissions_sum,
     label='hospitalised',
     color='darkblue',
 )
 
 
 new_deaths_sum = Series(
-    metric='newDeaths28DaysByDeathDateRollingSum',
+    metric=c.new_deaths_sum,
     label='died',
     color='black',
 )
 
 
 unique_people_tested_sum = Series(
-    metric='uniquePeopleTestedBySpecimenDateRollingSum',
+    metric=c.unique_people_tested_sum,
     label='tested',
     color='darkgreen',
 )
 
 first_dose_weekly = Series(
-    metric='weeklyPeopleVaccinatedFirstDoseByVaccinationDate',
+    metric=c.first_dose_weekly,
     label='First Dose (New)',
 )
 
 second_dose_weekly = Series(
-    metric='weeklyPeopleVaccinatedSecondDoseByVaccinationDate',
+    metric=c.second_dose_weekly,
     label='Second Dose (New)',
 )
 
 first_dose_daily_new = Series(
-    metric="newPeopleVaccinatedFirstDoseByPublishDate",
+    metric=c.first_dose_daily_new,
     label='First Dose (New)',
 )
 
 second_dose_daily_new = Series(
-    metric="newPeopleVaccinatedSecondDoseByPublishDate",
+    metric=c.second_dose_daily_new,
     label='Second Dose (New)',
 )
 
 complete_dose_daily_new = Series(
-    metric="newPeopleVaccinatedCompleteByPublishDate",
+    metric=c.complete_dose_daily_new,
     label='Complete (New)',
 )
 
 first_dose_weekly_cum = Series(
-    metric='cumPeopleVaccinatedFirstDoseByVaccinationDate',
+    metric=c.first_dose_weekly_cum,
     label='First Dose (Total)',
 )
 
 second_dose_weekly_cum = Series(
-    metric='cumPeopleVaccinatedSecondDoseByVaccinationDate',
+    metric=c.second_dose_weekly_cum,
     label='Second Dose (Total)',
 )
 
 first_dose_daily_cum = Series(
-    metric="cumPeopleVaccinatedFirstDoseByPublishDate",
+    metric=c.first_dose_daily_cum,
     label='First Dose (Total)'
 )
 
 second_dose_daily_cum = Series(
-    metric="cumPeopleVaccinatedSecondDoseByPublishDate",
+    metric=c.second_dose_daily_new,
     label='Second Dose (Total)',
 )
 
 complete_dose_daily_cum = Series(
-    metric="cumPeopleVaccinatedCompleteByPublishDate",
+    metric=c.complete_dose_daily_cum,
     label='Complete (Total)',
 )
