4a490419602bbedf91e70defde0d83d29fa6ee78
Chris Withers <chris@simplistix.co.uk>
2021-02-04 10:58:21 +0000

    refactor animated maps into one file

--- animated.py
+++ animated.py
@@ -1,28 +1,17 @@
 import concurrent.futures
-from argparse import ArgumentParser
-from datetime import timedelta
 from functools import partial
 from os import cpu_count
 from shutil import rmtree
 from typing import Union
 
 import imageio
-import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-from matplotlib.colors import SymLogNorm
-from matplotlib.ticker import StrMethodFormatter
 from moviepy.video.VideoClip import ImageClip
 from moviepy.video.compositing.concatenate import concatenate_videoclips
 from pygifsicle import optimize
 from tqdm import tqdm
 
-from args import add_date_arg
-from constants import output_path, second_wave, area_code
-from geo import views, ltla_geoms, View, above
-from phe import plot_summary
-import series as s
-from plotting import show_area
+from constants import output_path
 
 
 def safe_render(render, image_path, raise_errors, item):
@@ -110,97 +99,6 @@ def round_nearest(a, nearest):
     return (a/nearest).round(0) * nearest
 
 
-def render_map(ax, frame_date, read_map_data, view: View, column, title,
-               vmin, linthresh, vmax, linticks, logticks,
-               linnearest=1, lognearest=1,
-               load_geoms=ltla_geoms, cmap='inferno_r',
-               label_top_5=False,
-               **plot_kw):
-    df, _ = read_map_data()
-    dt = frame_date.date()
-    data = df.loc[dt]
-
-    current_pct_geo = pd.merge(load_geoms(), data, how='outer', left_on='code',
-                               right_on=area_code)
-
-    ticks = np.concatenate((
-        round_nearest(np.linspace(vmin, linthresh, linticks), linnearest),
-        round_nearest(np.geomspace(linthresh, vmax, logticks), lognearest))
-    )
-
-    legend_kwds = {
-        'fraction': view.legend_fraction,
-        'format': StrMethodFormatter('{x:,.0f}'),
-        'ticks': ticks,
-    }
-    legend_kwds.update(plot_kw.pop('legend_kwds', {}))
-
-    plot_kw['missing_kwds'] = plot_kw.pop('missing_kwds', {'color': 'lightgrey'})
-
-    ax = current_pct_geo.plot(
-        ax=ax,
-        column=column,
-        legend=True,
-        norm=SymLogNorm(linthresh=linthresh, vmin=vmin, vmax=vmax, base=10),
-        cmap=cmap,
-        vmin=vmin,
-        vmax=vmax,
-        legend_kwds=legend_kwds,
-        **plot_kw
-    )
-    show_area(ax, view)
-    ax.set_title(title)
-
-    for places in view.outline:
-        places.frame().geometry.boundary.plot(
-            ax=ax, edgecolor=places.colour, linewidth=places.outline_width
-        )
-
-    for places in view.label:
-        frame = places.frame()
-        for name, geometry in zip(frame['name'], frame['geometry']):
-            ax.annotate(
-                name,
-                xy=places.label_location(geometry),
-                ha='center',
-                fontsize=places.fontsize,
-                fontweight=places.fontweight,
-                color=places.colour
-            )
-
-    if label_top_5:
-        top_5 = current_pct_geo.sort_values(column, ascending=False).iloc[:5]
-        for name, geometry in zip(top_5['name'], top_5['geometry']):
-            ax.annotate(
-                name,
-                xy=above(geometry),
-                ha='center',
-                fontsize='x-large',
-            )
-
-
-def render_dt(
-        data_date, earliest_date, to_date, dpi, render_map, view, frame_date, image_path
-):
-    view = views[view]
-    width, height, height_ratio = view.layout()
-    fig, (map_ax, lines_ax) = plt.subplots(
-        figsize=(width, height),
-        nrows=2,
-        gridspec_kw={'height_ratios': [height_ratio, 1], 'hspace': view.grid_hspace}
-    )
-    render_map(map_ax, frame_date, view)
-    plot_summary(lines_ax, data_date, frame_date, earliest_date, to_date,
-                 series=(s.new_admissions_sum, s.new_deaths_sum), title=False)
-    fig.text(0.25, 0.07,
-             f'@chriswithers13 - '
-             f'data from https://coronavirus.data.gov.uk/ retrieved on {data_date:%d %b %Y}',
-             color='darkgrey',
-             zorder=-1)
-    plt.savefig(image_path / f'{frame_date.date()}.png', dpi=dpi, bbox_inches='tight')
-    plt.close()
-
-
 def slowing_durations(dates, normal=0.05, slow=0.3, period=30):
     durations = np.full((len(dates)), normal)
     period = min(len(durations), period)
@@ -208,35 +106,3 @@ def slowing_durations(dates, normal=0.05, slow=0.3, period=30):
     return list(durations)
 
 
-def map_main(name, read_map_data, render_map, default_view='uk', default_exclude=0, dpi=90):
-    parser = ArgumentParser()
-    add_date_arg(parser, default=second_wave)
-
-    group = parser.add_mutually_exclusive_group()
-    group.add_argument('--exclude-days', default=default_exclude, type=int)
-    add_date_arg(group, '--to-date')
-
-    parser.add_argument('--output', default='mp4')
-    parser.add_argument('--ignore-errors', dest='raise_errors', action='store_false')
-    parser.add_argument('--view', choices=views.keys(), default=default_view)
-    parser.add_argument('--max-workers', type=int)
-    parser.add_argument('--duration', type=float, help='fast=0.05, slow=0.3')
-    add_date_arg(group, '--single')
-    args = parser.parse_args()
-
-    views[args.view].check()
-
-    df, data_date = read_map_data()
-
-    to_date = args.to_date or df.index.max().date() - timedelta(days=args.exclude_days)
-    earliest_date = df.index.min().date()
-    dates = pd.date_range(args.from_date, to_date)
-
-    render = partial(
-        render_dt, data_date, earliest_date, to_date, dpi, render_map, args.view
-    )
-
-    duration = args.duration or slowing_durations(dates)
-    parallel_render(name+'-'+args.view, render, dates, duration,
-                    args.output, raise_errors=args.raise_errors, max_workers=args.max_workers,
-                    item=pd.to_datetime(args.single) if args.single else None)
--- animated_map.py
+++ animated_map.py
@@ -1,35 +1,239 @@
-from functools import lru_cache
+from argparse import ArgumentParser
+from copy import copy
+from dataclasses import dataclass, replace
+from datetime import timedelta
+from functools import lru_cache, partial, cached_property
 
-from animated import map_main, render_map
-from constants import per100k, date_col, area_code
-from phe import with_population, best_data
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+from matplotlib.cm import get_cmap
+from matplotlib.colors import SymLogNorm
+from matplotlib.ticker import StrMethodFormatter
 
-rolling_days = 14
+import series as s
+from animated import round_nearest, slowing_durations, parallel_render
+from args import add_date_arg
+from constants import per100k, date_col, area_code, ltla, second_wave, msoa, base_path, \
+    release_timestamp
+from geo import View, above, views, area_type_to_geoms
+from plotting import show_area
+from series import Series
+from phe import with_population, best_data, plot_summary, read_csv
 
 
-@lru_cache
-def read_map_data():
-    df, data_date = best_data()
-    df = with_population(df)
-    pivoted = df.pivot_table(values=per100k, index=[date_col], columns=area_code)
-    smoothed = pivoted.fillna(0).rolling(rolling_days).mean()
-    return smoothed.unstack().reset_index(name=per100k).set_index(date_col), data_date
+def render_map(ax, frame_date, map: 'Map', view: View, label_top_5=False, column=per100k):
 
+    cmap = copy(get_cmap(map.cmap))
+    cmap.set_under(map.below_color)
+    r = map.range
 
-def render_cases_map(ax, frame_date, view):
-    render_map(
-        ax, frame_date, read_map_data, view, column=per100k,
-        title=f'COVID-19 cases as of {frame_date:%d %b %Y}',
-        vmin=0, linthresh=30, vmax=200, linticks=4, logticks=5, lognearest=10,
+    df, _ = map.data
+    dt = frame_date.date()
+    data_for_dt = df.loc[dt]
+
+    data = pd.merge(area_type_to_geoms[map.area_type](), data_for_dt,
+                    how='outer', left_on='code', right_on=area_code)
+
+    ticks = np.concatenate((
+        round_nearest(np.linspace(r.vmin, r.linthresh, r.linticks), r.linnearest),
+        round_nearest(np.geomspace(r.linthresh, r.vmax, r.logticks), r.lognearest))
+    )
+
+    ax = data.plot(
+        ax=ax,
+        column=column,
+        legend=True,
+        norm=SymLogNorm(linthresh=r.linthresh, vmin=r.vmin, vmax=r.vmax, base=10),
+        cmap=cmap,
+        vmin=r.vmin,
+        vmax=r.vmax,
+        antialiased=map.antialiased,
+        missing_kwds={'color': 'lightgrey'},
         legend_kwds={
-            'extend': 'max',
-            'label': f'{rolling_days} day rolling average of new cases per 100,000 people '
-                     f'by specimen date'
-        })
+            'fraction': view.legend_fraction,
+            'format': StrMethodFormatter('{x:,.0f}'),
+            'ticks': ticks,
+            'extend': 'max' if map.range.vmin == 0 else 'both',
+            'label': map.axis_label()
+        },
+    )
+    show_area(ax, view)
+    ax.set_title(f'COVID-19 {map.series.label} as of {frame_date:%d %b %Y}')
+
+    for places in view.outline:
+        places.frame().geometry.boundary.plot(
+            ax=ax, edgecolor=places.colour, linewidth=places.outline_width
+        )
+
+    for places in view.label:
+        frame = places.frame()
+        for name, geometry in zip(frame['name'], frame['geometry']):
+            ax.annotate(
+                name,
+                xy=places.label_location(geometry),
+                ha='center',
+                fontsize=places.fontsize,
+                fontweight=places.fontweight,
+                color=places.colour
+            )
+
+    if label_top_5:
+        top_5 = data.sort_values(column, ascending=False).iloc[:5]
+        for name, geometry in zip(top_5['name'], top_5['geometry']):
+            ax.annotate(
+                name,
+                xy=above(geometry),
+                ha='center',
+                fontsize='x-large',
+            )
+
+
+def render_dt(data_date, earliest_date, to_date, area_type, map_type, view, frame_date, image_path):
+    map = get_map(area_type, map_type)
+    view = views[view]
+    width, height, height_ratio = view.layout()
+    fig, (map_ax, lines_ax) = plt.subplots(
+        figsize=(width, height),
+        nrows=2,
+        gridspec_kw={'height_ratios': [height_ratio, 1], 'hspace': view.grid_hspace}
+    )
+    render_map(map_ax, frame_date, map, view)
+    plot_summary(lines_ax, data_date, frame_date, earliest_date, to_date,
+                 series=(s.new_admissions_sum, s.new_deaths_sum), title=False)
+    fig.text(0.25, 0.07,
+             f'@chriswithers13 - '
+             f'data from https://coronavirus.data.gov.uk/ retrieved on {data_date:%d %b %Y}',
+             color='darkgrey',
+             zorder=-1)
+    plt.savefig(image_path / f'{frame_date.date()}.png', dpi=map.dpi, bbox_inches='tight')
+    plt.close()
 
 
 def main():
-    map_main('animated_map_ltla_cases', read_map_data, render_cases_map, default_exclude=5)
+    parser = ArgumentParser()
+    parser.add_argument('area_type', choices=MAPS.keys())
+    parser.add_argument('map')
+    add_date_arg(parser, default=second_wave)
+
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('--exclude-days', type=int)
+    add_date_arg(group, '--to-date')
+
+    parser.add_argument('--output', default='mp4')
+    parser.add_argument('--ignore-errors', dest='raise_errors', action='store_false')
+    parser.add_argument('--view', choices=views.keys())
+    parser.add_argument('--max-workers', type=int)
+    parser.add_argument('--duration', type=float, help='fast=0.05, slow=0.3')
+    add_date_arg(group, '--single')
+    args = parser.parse_args()
+
+    map = get_map(args.area_type, args.map)
+    view = args.view or map.default_view
+    views[view].check()
+
+    df, data_date = map.data
+
+    exclude_days = map.default_exclude if args.exclude_days is None else map.default_exclude
+    to_date = args.to_date or df.index.max().date() - timedelta(days=exclude_days)
+    earliest_date = df.index.min().date()
+    dates = pd.date_range(args.from_date, to_date)
+
+    render = partial(render_dt, data_date, earliest_date, to_date, args.area_type, args.map, view)
+
+    duration = args.duration or slowing_durations(dates)
+    parallel_render(f'animated_map_{map.area_type}_{map.series.label}_{view}',
+                    render, dates, duration,
+                    args.output, raise_errors=args.raise_errors, max_workers=args.max_workers,
+                    item=pd.to_datetime(args.single) if args.single else None)
+
+
+@dataclass
+class Range:
+    vmin: int = 0
+    vmax: int = None
+    linthresh: int = None
+    linticks: int = 4
+    linnearest: float = 1
+    logticks: int = 5
+    lognearest: float = 1
+
+
+@dataclass
+class Map:
+    series: Series
+    range: Range
+    default_exclude: int = 5
+    default_view: str = 'uk'
+    dpi: int = 90
+    rolling_days: int = 14
+    cmap: str = 'inferno_r'
+    below_color: str = 'lightgrey'
+    missing_color: str = 'grey'
+    antialiased: bool = True
+    area_type: str = None
+
+    def axis_label(self):
+        return f'{self.rolling_days} day rolling average of {self.series.title} per 100,000 people'
+
+    def for_area_type(self, area_type):
+        return replace(self, area_type=area_type)
+
+    @cached_property
+    def data(self):
+        df, data_date = best_data(area_type=self.area_type)
+        df = with_population(df, source_cols=(self.series.metric,))
+        pivoted = df.pivot_table(values=per100k, index=[date_col], columns=area_code)
+        smoothed = pivoted.fillna(0).rolling(self.rolling_days).mean()
+        return smoothed.unstack().reset_index(name=per100k).set_index(date_col), data_date
+
+
+@dataclass
+class MSOAMap(Map):
+
+    default_exclude: int = 0
+    default_view: str = 'england'
+    dpi: int = 150
+    rolling_days: int = 7
+    missing_color: str = 'white'
+    antialiased: bool = False
+
+
+    def axis_label(self):
+        return self.series.title
+
+    @cached_property
+    def data(self):
+        assert self.area_type == msoa
+        assert self.rolling_days == 7
+        data = read_csv(base_path / 'msoa_composite.csv', index_col=date_col)
+        return (
+            data.fillna(0).rename(columns={self.series.metric: per100k}),
+            pd.to_datetime(data.iloc[-1][release_timestamp])
+        )
+
+
+@lru_cache
+def get_map(area_type: str, map_type: str) -> Map:
+    return MAPS[area_type][map_type].for_area_type(area_type)
+
+
+MAPS = {
+    ltla: {
+        'cases': Map(
+            s.new_cases,
+            Range(vmin=0, linthresh=30, vmax=200,
+                  linticks=4, logticks=5, lognearest=10)
+        )
+    },
+    msoa: {
+        'cases': MSOAMap(
+            s.new_cases_rate,
+            Range(vmin=30, linthresh=700, vmax=4000,
+                  linticks=7, linnearest=10, logticks=5, lognearest=100)
+        )
+    },
+}
 
 
 if __name__ == '__main__':
