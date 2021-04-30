7b4b518444c81e338c0be17d2cd0f3060d4f296d
Chris Withers <chris@simplistix.co.uk>
2021-04-30 14:42:48 +0100

    Allow data_date to be specified if the necessary ones aren't all available.

--- animated_demographics.py
+++ animated_demographics.py
@@ -12,9 +12,9 @@ from plotting import per0_formatter, pct_formatter
 
 
 @lru_cache
-def data_for(series, earliest, pct: bool):
+def data_for(data_date, series, earliest, pct: bool):
     bars = Bars.get(series, earliest=earliest)
-    data, data_date = bars.data_for('*')
+    data, data_date = bars.data_for(data_date)
     if pct:
         data = data.divide(data.sum(axis='columns'), axis='rows')
     data = data.rolling(7).mean().dropna(how='all')
@@ -23,9 +23,9 @@ def data_for(series, earliest, pct: bool):
 
 def plot_demographic_bars(
         bars: BarsLookup, dt: pd.Timestamp, from_date: pd.Timestamp, ax:
-        Axes, max_x: int = None, pct: bool = False
+        Axes, max_x: int = None, pct: bool = False, data_date='*',
 ):
-    data, _, bars = data_for(bars, earliest=from_date, pct=pct)
+    data, _, bars = data_for(data_date, bars, earliest=from_date, pct=pct)
     data.loc[dt].plot.barh(ax=ax, width=0.9)
     ax.get_yaxis().label.set_visible(False)
     label = bars.series.label.capitalize()
@@ -40,13 +40,14 @@ def plot_demographic_bars(
 
 def plot_date(dt, *,
               series, maxes, from_date, pct: bool = False,
-              image_path=None, dpi=100):
+              image_path=None, dpi=100, data_date='*'):
     dt = pd.to_datetime(dt)
     fig, axes = plt.subplots(nrows=1, ncols=len(series),
                              figsize=(16, 10), constrained_layout=True)
     fig.suptitle(f'PHE data for {dt:%d %b %y}', fontsize=20)
     for i, s in enumerate(series):
-        plot_demographic_bars(s, dt, from_date, axes[i], max_x=maxes[s], pct=pct)
+        plot_demographic_bars(s, dt, from_date, axes[i],
+                              max_x=maxes[s], pct=pct, data_date=data_date)
 
     if image_path:
         plt.savefig(image_path / f'{dt.date()}.png', bbox_inches='tight', dpi=dpi)
@@ -57,6 +58,7 @@ def main():
     parser = ArgumentParser()
     add_parallel_args(parser, default_output='gif', from_date='2020-03-15')
     parser.add_argument('--pct', action='store_true')
+    parser.add_argument('--data-date', default='*')
     args = parser.parse_args()
 
     data_dates = set()
@@ -64,12 +66,13 @@ def main():
     maxes = {}
     series = 'deaths_demographics_for_comparison', 'cases_demographics'
     for s in series:
-        data, data_date, _ = data_for(s, earliest=args.from_date, pct=args.pct)
+        data, data_date, _ = data_for(args.data_date, s, earliest=args.from_date, pct=args.pct)
         data_dates.add(data_date)
         dates = data.index.values
         maxes[s] = max(data.max())
 
-    assert len(data_dates) == 1, data_dates
+    if len(data_dates) != 1:
+        parser.error('--data-date, pick one: '+', '.join(str(d) for d in sorted(data_dates)))
 
     name = 'animated_demographics'
     if args.pct:
@@ -81,6 +84,7 @@ def main():
                         maxes=maxes,
                         from_date=args.from_date,
                         pct=args.pct,
+                        data_date=args.data_date
                     ),
                     dates, **parallel_params(args))
 
