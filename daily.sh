51c1e1d1a551cd51778f75287bb424d3c4a333fe
Chris Withers <chris@simplistix.co.uk>
2022-04-22 08:21:23 +0100

    drop daily zoe downloads

--- daily.sh
+++ daily.sh
@@ -3,5 +3,4 @@ PYTHON=/Users/chris/miniconda3/envs/coronavirus/bin/python
 cd /Users/chris/coronavirus/
 $PYTHON download.py daily
 $PYTHON msoa_download.py
-$PYTHON zoe.py
 $PYTHON msoa_check.py --composite
